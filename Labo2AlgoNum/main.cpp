3bbddd5d0e8b084bb3826c449144406dd2803145
Kevin Vulliemin <kevin.vulliemin@he-arc.ch>
2015-03-18 15:18:54 +0100

    Externalisation des fonctions Réalisation d'un petit accueil, à finir

--- Labo2AlgoNum/main.cpp
+++ Labo2AlgoNum/main.cpp
@@ -14,6 +14,7 @@
 //      Librairies      //
 //*--------------------*//
 #include <iostream>
+#include <sstream>
 #include <math.h>
 
 using namespace std;
@@ -21,8 +22,9 @@ using namespace std;
 //*--------------------*//
 //      Définition      //
 //*--------------------*//
+int askInput(std::string message, int value);
 float fun(float x);
-void bisection(float a, float b, float eps);
+void bissection(float a, float b, float eps);
 
 //val 1 pour la première fonction et val 2 pour la 2ème
 #define val 1
@@ -32,9 +34,65 @@ void bisection(float a, float b, float eps);
 //*--------------------*//
 int main()
 {
-    float tempa, a = -100;  //Valeurs d'intervalles
-    float tempb, b = 100;
+    //Valeurs par défaut
+    float a = -100;         //Valeurs d'intervalles
+    float b = 100;
     float eps = 0.0001;     //Précision
+
+    //Accueil et demandes utilisateurs
+    cout << "Bonjour, ce programme propose a l'utilisateur de trouver" << endl <<
+            "les points d'intersections d'une fonction avec l'axe des x.\n" << endl;
+    do
+    {
+        a = askInput("Veuillez entrer le debut de l'intervalle", -100);
+        b = askInput("Veuiller entrer la fin de l'intervalle", 100);
+
+        //Résolution
+        bissection(a, b, eps);
+    }while(false);
+
+    return 0;
+}
+
+//*--------------------*//
+//      Fonctions       //
+//*--------------------*//
+float fun(float x)
+{
+     switch(val)
+     {
+     case 1:
+        {
+        return sin(x) - x / 13;
+        }
+     case 2:
+        {
+        return x / (1 - pow(x, 2.0));
+        }
+     default:
+        {
+        cout << "Val must be 1 or 2" << endl;
+        }
+     }
+}
+
+int askInput(std::string message, int value)
+{
+    cout << message << " (" << value << " par defaut) : " << endl;
+    std::string input;
+    std::getline( std::cin, input );
+
+    if ( !input.empty() ) {
+        std::istringstream stream( input );
+        stream >> value;
+    }
+
+    return value;
+}
+
+void bissection(float a, float b, float eps)
+{
+    float tempa, tempb;     //Valeurs d'intervalles
     float m;                //Moyenne
     float fm, fa, fb;       //Y des valeurs relatives
     int i = 0;              //Compteur de solutions
@@ -82,28 +140,4 @@ int main()
 
     //Pas de solutions
     if(fm > eps) cout << "Pas de solution pour cette intervale" << endl;
-
-    return 0;
-}
-
-//*--------------------*//
-//      Fonctions       //
-//*--------------------*//
-float fun(float x)
-    {
-     switch(val)
-     {
-     case 1:
-        {
-        return sin(x) - x / 13;
-        }
-     case 2:
-        {
-        return x / (1 - pow(x, 2.0));
-        }
-     default:
-        {
-        cout << "Val must be 1 or 2" << endl;
-        }
-     }
 }
